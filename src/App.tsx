ec90c7b0530e6ea41d56bdabd290982e7801b146
Kiri1111 <92_medved@mail.ru>
2023-07-12 10:19:19 +0300

    feat: chat logic with socket.io

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {io} from "socket.io-client";
+
+const socket = io("https://samurai-chat-back.herokuapp.com")
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	useEffect(() => {
+		socket.on('init-messages-published', (messages: any) => {
+			setMessages(messages)
+		})
+		socket.on('new-message-sent', (message: any) => {
+			setMessages((messages) => [...messages, message])
+		})
+	}, [])
+
+	const [messages, setMessages] = useState<any[]>([])
+	const [message, setMessage] = useState('')
+	const [name, setName] = useState('Ker9')
+
+	return (
+		<div className="App">
+			<div style={{marginBottom: '40px', border: '1px solid black', width: '400px', height: '500px'}}>
+				{messages.map(el => {
+					return <div key={el.id}>
+						<b>{el.user.name}:</b>
+						{el.message}
+						<hr/>
+					</div>
+				})}
+			</div>
+			<input value={name} onChange={(e) => {
+				setName(e.currentTarget.value)
+			}}/>
+			<button style={{marginBottom: '40px'}} onClick={() => {
+				socket.emit('client-name-sent', name)
+			}}>send name
+			</button>
+			<textarea value={message} onChange={(e) => setMessage(e.currentTarget.value)}></textarea>
+			<button onClick={() => {
+				socket.emit('client-message-sent', message)
+			}}>send message
+			</button>
+		</div>
+	);
 }
 
 export default App;
